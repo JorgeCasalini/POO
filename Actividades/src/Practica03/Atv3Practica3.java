ae579c951cd059d2c900b7f1fbaaabb9386e95a9
jorgeCasalini <joshcasalini@gmail.com>
2024-10-16 10:10:29 -0300

    Resolve problem raised in Atv3, together with the Estanteria class

--- Actividades/src/Practica03/Atv3Practica3.java
+++ Actividades/src/Practica03/Atv3Practica3.java
@@ -14,7 +14,21 @@ import PaqueteLectura.*;
 public class Ejercicio3 {
     
     public static void main(String[] args) {
-        
+        Estanteria estante1 = new Estanteria();
+        GeneradorAleatorio.iniciar();
+            
+        int i;
+        for(i=0;i<10;i++){
+            Autor unAutor = new Autor(GeneradorAleatorio.generarString(8), GeneradorAleatorio.generarString(10), GeneradorAleatorio.generarString(5));
+            Libro libro = new Libro(GeneradorAleatorio.generarString(5), GeneradorAleatorio.generarString(4),GeneradorAleatorio.generarInt(4), unAutor ,GeneradorAleatorio.generarString(4),GeneradorAleatorio.generarDouble(6));
+            if(i == 5){
+                libro.setTitulo("mujercitas");
+            }
+            estante1.agregarLibro(libro);
+        }
+        System.out.println(estante1.getCantLibros());
+        System.out.println(estante1.estaLleno());
+        System.out.println(estante1.devolverTitulo("mujercitas").getPrimerAutor().getNombre());
     }
     
 }
